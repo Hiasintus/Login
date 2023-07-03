6760b6fe6cc4ea9abcdcdc8de9c059bd2369614e
Hiasintus <hiasintusguido@gmail.com>
2023-07-03 22:23:18 +0700

    Initial commit

---
+++ app/src/main/java/com/sintus/login/MenuActivity.java
@@ -0,0 +1,35 @@
+package com.sintus.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MenuActivity extends AppCompatActivity {
+    private  Button _tampilMahasiswaButton;
+    private Intent _tampilMahasiswaIntent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        initTampilMahasiswa();
+    }
+
+    private void initTampilMahasiswa()
+    {
+        _tampilMahasiswaButton = findViewById(R.id.tampilMahasiswaButton);
+
+        _tampilMahasiswaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                _tampilMahasiswaIntent = new Intent(getApplicationContext(), TampilMahasiswaActivity.class);
+                startActivity(_tampilMahasiswaIntent);
+            }
+        });
+    }
+}
